2fb501c34f795b6a81f2c1fa123d2dd5ea7dc704
hangerbro <crazy_hangers83@yahoo.com>
2017-04-02 12:56:04 -0400

    added basic apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,14 +5,14 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #install apache package
 
-package 'apache2' do
+package 'apache' do
          package_name 'httpd'
 	 action :install
 end
 
-service 'apache2' do
+service 'apache' do
 	 service_name 'httpd'
 	 action [:start, :enable]
 end
 
-
+include_recipe 'apache::websites'
